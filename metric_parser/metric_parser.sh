50f4ea7c96fcdfcaa3b02dee40af6f857b749269
noamteyssier <nb.teyssier@gmail.com>
2019-01-14 14:04:28 -0800

    add filename

--- metric_parser/metric_parser.sh
+++ metric_parser/metric_parser.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# input first arg or stdin
-[ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
+fn=$1
 
-fgrep 'GENOME_TERRITORY' -A 1 | fgrep 'GENOME' -v 
+fgrep 'GENOME_TERRITORY' -A 1 $fn | fgrep 'GENOME' -v | sed "s/$/	$fn/" 
